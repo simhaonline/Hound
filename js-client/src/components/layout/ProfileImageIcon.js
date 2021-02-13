a6a99414c94253e839f85ddec7f8a585d27bda20
lukajose <lukajosegamulin@outlook.com>
2021-02-13 17:23:26 +1100

    add profile to house card

--- js-client/src/components/layout/ProfileImageIcon.js
+++ js-client/src/components/layout/ProfileImageIcon.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function ProfileImageIcon({srcImg}) {
+function ProfileImageIcon({srcImg, size = 20, label='Profile'}) {
     return (
         <div className={"profile-pic-icon"}> 
-            <img src={srcImg} />
-            <span> Profile </span>
+            <img src={srcImg} style={{width:size+'px',height:size + 'px'}}/>
+            {label && <span>{label}</span>}
         </div>
     )
 }
--- js-client/src/components/utils/HouseCard.js
+++ js-client/src/components/utils/HouseCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from '../../images/33darl2.jpg'
-
-function HouseCard({bedrooms = 0,bathrooms = 0,garage = 0,type = 'Any', price= '3,000,000'}) {
+import ProfileImageIcon from '../layout/ProfileImageIcon'
+import UserImg from '../../images/Tommy.jpg'
+function HouseCard({bedrooms = 0,bathrooms = 0,garage = 0,type = 'Any', price= '3,000,000' }) {
     return (
         <div className={"feeds-container"}>
             <div className={"flex-row"}>
@@ -11,7 +12,10 @@ function HouseCard({bedrooms = 0,bathrooms = 0,garage = 0,type = 'Any', price= '
             </div>
             <div className={"flex-row"}>
                     <div className={"house-info"}>
-                        <div className={"house-price"}> $ {price} AUD</div>
+                    <div className={"house-props-items"}>
+                        <div className={"house-item"}><ProfileImageIcon srcImg={UserImg} label={false} size={50}/></div>
+                        <div className={"house-item"}><div className={"house-price"}> $ {price} AUD</div></div>
+                    </div>
                     <div className={"house-props-items"}>
                         <div className={"house-item"}>Type {type}</div>
                         <div className={"house-item"}>Bedrooms {bedrooms}</div>
